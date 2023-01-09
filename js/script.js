8115b4122cf7a884d5e83608dd5dcd5dfc8adf27
RoninsWork <yurkaronin@gmail.com>
2023-01-09 17:11:15 +0300

    адаптив и правки

--- js/script.js
+++ js/script.js
@@ -2,19 +2,24 @@
 gsap.registerPlugin(TextPlugin);
 
 const imgGroup = document.querySelector('.hero');
-
-imgGroup.addEventListener("mousemove", parallax);
-
 function parallax(e) {
   this.querySelectorAll('.layer').forEach(layer => {
     const speed = layer.getAttribute('data-speed');
-
+    console.log('Событие есть!');
     const x = (window.innerWidth - e.pageX * speed)/100;
     const y = (window.innerHeight - e.pageY * speed)/100;
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   })
 };
+
+if(screen.width < 1024) {
+  imgGroup.addEventListener("mousemove", parallax);
+}
+
+
+
+
 
 
 //replaces yourElement's text with "This is the new text" over the course of 2 seconds
